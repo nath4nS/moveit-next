709f83be33f457c5f1c68cf395ff6d7254eb5fc3
nath4ns <nathansousa360@gmail.com>
2021-02-25 19:34:49 -0300

    Terceira aula

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,17 +1,23 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export function ExperienceBar() {
+    const { currentExperience, experienceToNevelLevel } = useContext(ChallengesContext);
+
+    const percentToNevelLevel = Math.round(currentExperience * 100) / experienceToNevelLevel;
+
     return (
         <header className={styles.experienceBar}>
             <span>0 xp</span>
                 <div>
-                    <div style={{ width: '60%' }}/>
+                    <div style={{ width: `${percentToNevelLevel}` }}/>
 
-                    <span className={styles.currentExperience} style={{ left: '50%' }}>
-                        300 xp
+                    <span className={styles.currentExperience} style={{ left: `${percentToNevelLevel}` }}>
+                        {currentExperience} xp
                     </span>
                 </div>
-            <span>600 xp</span>
+            <span>{experienceToNevelLevel} xp</span>
         </header>
     );
 }
